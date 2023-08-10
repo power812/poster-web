85e44bd16ea3773eb20379f311df0f0ce39989f9
power <1410837981@qq.com>
2023-08-10 20:41:16 +0800

    feat: hash模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useUserStore } from '../store/user'
 import axios from 'axios'
 import { message } from 'ant-design-vue'
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {
             path: '/',
