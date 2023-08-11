87ea8ad74644cc5e98b6b9c133dd46356c8d075a
power <1410837981@qq.com>
2023-08-11 15:19:21 +0800

    s

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig((config) => {
-    const base = config.command === 'build' ? 'https://power-cli.oss-cn-shenzhen.aliyuncs.com/' : '/'
+    const base = config.command === 'build' ? 'https://power-cli.oss-cn-shenzhen.aliyuncs.com/poster-web/' : '/'
     if (config.command === 'build') {
         process.env.NODE_ENV = 'production'
     }
